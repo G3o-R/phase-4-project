88e225819bc6f7ff1f5394955cbb6aa417368f39
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-08-02 14:39:03 -0700

    change create_descriptions to create_jobs

--- db/migrate/20230802212722_create_jobs.rb
+++ db/migrate/20230802212722_create_jobs.rb
@@ -1,6 +1,7 @@
-class CreateDescriptions < ActiveRecord::Migration[6.1]
+class CreateJobs < ActiveRecord::Migration[6.1]
   def change
-    create_table :descriptions do |t|
+    create_table :jobs do |t|
+      t.string :description
       t.float :pay
       t.string :location
       t.string :position
