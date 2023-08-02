dc31f4f56f3649615b4688c0689f364da4524760
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-08-02 14:48:42 -0700

    build Job model

---
+++ app/models/job.rb
@@ -0,0 +1,9 @@
+class Job < ApplicationRecord
+    has_many :applications
+    has_many :applicants, through: :applications, source: :user
+  
+    validates :description, presence: true
+    validates :pay, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :location, presence: true
+    validates :position, presence: true
+  end
