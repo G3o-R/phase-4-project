fd39c9392eb6ba9ba73df6ca6ba7bfe8eead9a72
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-08-09 14:18:29 -0700

    add not_found response to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+
+  private
+
+  def record_not_found_response
+    render json: {errors: "model not found"}, status: :not_found
+  end
+
 end
