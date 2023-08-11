8a4ca3c68fdb8bf95fde657af9345ff103fc4b49
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-08-11 15:10:48 -0700

    add create to JobApplicationController

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,5 +1,10 @@
 class JobApplicationsController < ApplicationController
     def create
+        job_applications = JobApplication.create!(application_params)
+    end
 
+    private
+    def application_params
+        params.permit(:email, :phone_number, :user_id, :job_id)
     end
 end
