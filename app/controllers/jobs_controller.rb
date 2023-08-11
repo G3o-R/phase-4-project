0706d8b3c35422f8906ba88e46e37226f2aaca0b
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-08-11 11:53:05 -0700

    add create to jobs_controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,15 +1,27 @@
 class JobsController < ApplicationController
+    def create
+        new_job = Job.create!(job_params)
+        render json: new_job, status: :created
+    end
+    
     def index
         jobs = Job.all
         render json: jobs
     end
+    
     def show 
         job = Job.find(params[:id])
         render json: job
     end
+    
     def destroy
         job = Job.find(params[:id])
         job.destroy
         head :no_content
+    end
+
+    private
+    def job_params
+        params.permit(:description,:pay,:location,:position,:company)
     end
 end
