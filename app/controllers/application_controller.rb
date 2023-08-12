9345eee0fb49c7e047d189a7715fa367328e4b41
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-08-12 15:39:23 -0700

    add authorize to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,14 @@ class ApplicationController < ActionController::API
 
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
+  before_action :authorize
+
   private
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  end
 
   def record_not_found_response
     render json: {errors: "model not found"}, status: :not_found
