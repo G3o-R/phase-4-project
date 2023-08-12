4e882f988ba42ccb948fd80f7ad8884c415ad016
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-08-11 19:12:17 -0700

    build JobApplicationController

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -2,9 +2,23 @@ class JobApplicationsController < ApplicationController
     def create
         job_applications = JobApplication.create!(application_params)
     end
+    # read will be handled by an "includes" in user
+
+    def update 
+        job_application = Job.find(params[:id])
+        job_application.update(application_params)
+        rendder json: job_application
+    end
+
+    def destroy 
+        job_application = Job.find(params[:id])
+        job_application.destroy
+        head :no_content
+    end
 
     private
     def application_params
         params.permit(:email, :phone_number, :user_id, :job_id, :status)
     end
+
 end
