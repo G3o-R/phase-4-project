9b9050637a9e8024e9ceae27170b64541b087a07
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-08-20 18:06:43 -0700

    building login

--- client/src/components/LogInForm.js
+++ client/src/components/LogInForm.js
@@ -1,12 +1,33 @@
 import { useState } from "react"
 
 function LoginForm(){
+
+    const [loginFormData, setLoginForm] = useState({
+        username: "",
+        password: ""
+    })
+
+    const {password, username} = loginFormData
+
+    function handleChange(e){
+        let name = e.target.name
+        let value = e.target.value
+
+        setLoginForm({...loginFormData, [name]:value})
+        console.log(loginFormData)
+    }
+
+
     return(
         <div className="login" style={loginStyle}>
         <div style={formContainerStyle}>
-            <form>
-
+            <form style={formStyle}>
+                <label>Username</label>
+                <input type="text" name="username" value={username} placeholder="username..." onChange={handleChange}/>
+                <label>Password</label>
+                <input type="text" name="password" value={password} placeholder="password..." onChange={handleChange}/>
             </form>
+            
         </div>
         </div>
     )
@@ -25,6 +46,12 @@ const formContainerStyle ={
     aspectRatio:"5/8",
     justifySelf: "center",
     borderRadius: "10px 10px 10px 10px",
+}
+
+const formStyle = {
+    display:"flex",
+    flexDirection:"column",
+    alignItems:"center"
 }
 
 export default LoginForm
