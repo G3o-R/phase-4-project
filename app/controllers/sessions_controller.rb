0645e4a464da9f3a5b88e037c6cbb4b9569485e1
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-08-24 20:58:59 -0700

    building serilaizer for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 class SessionsController < ApplicationController
+    before_action :authorize
     skip_before_action :authorize, only: :create
+
+    def create 
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :jobs
   resources :applications
   post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
