06e3792fac5a6d2114f9d03d4478dac1fe561901
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-01 15:04:32 -0700

    building myJobCard

--- client/src/components/MyJobCard.js
+++ client/src/components/MyJobCard.js
@@ -1,9 +1,14 @@
 
 
 function MyJobCard({job_application}){
-    console.log(job_application)
+    const {company, description, email, location, phone_number, position, status } = job_application
+
     return (
         <div className="job-card">
+            <p>{status}</p>
+            <p>{position}</p>
+            <p>{company}</p>
+            <p>{location}</p>
         </div>
     )
 }
--- client/src/components/MyJobs.js
+++ client/src/components/MyJobs.js
@@ -1,13 +1,12 @@
 import MyJobCard from "./MyJobCard"
 
+
 function MyJobs({user}){
-   
-    console.log(user)
-    const displayMyJobCards = user.job_applications.map((job_application)=><MyJobCard job_application={job_application} key={job_application.id}/>)
+    const displayMyJobCards = !user ? null : user.job_applications.map((job_application)=><MyJobCard job_application={job_application} key={job_application.id}/>)
 
     return(
         <div className="my-jobs">
-            {!user ? <h1>Login and get hired!</h1>: (user.jobs == false ? <h1>Start Applying!!!</h1>: displayMyJobCards)}
+            {!user ? <h1>Login and get hired!</h1>: (user.job_applications == false ? <h1>Start Applying!!!</h1>: displayMyJobCards)}
         </div>
     )
 }
