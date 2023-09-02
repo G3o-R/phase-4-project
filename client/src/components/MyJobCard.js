668019029c1d003c9004e73d7de630150db3e7a7
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-01 17:06:32 -0700

    building MyJobs

--- client/src/components/MyJobCard.js
+++ client/src/components/MyJobCard.js
@@ -1,16 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
 
+const JobCardContainer = styled.div`
+  background-color: #ffffff;
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  margin: 10px auto;
+  max-width: 60rem; 
+  width: 100%; 
+  height:180px;
+  aspect-ratio: 5/3;
+  position: relative;
+`;
 
-function MyJobCard({job_application}){
-    const {company, description, email, location, phone_number, position, status } = job_application
+const JobStatus = styled.p`
+  font-size: 16px;
+  color: #333;
+  font-weight: bold;
+  background-color: #e0f0ff;
+  display: inline-block;
+  padding: 5px 10px;
+  border-radius: 5px;
+`;
 
-    return (
-        <div className="job-card">
-            <p>{status}</p>
-            <p>{position}</p>
-            <p>{company}</p>
-            <p>{location}</p>
-        </div>
-    )
-}
+const JobDescription = styled.p`
+  font-size: 14px;
+  color: #666;
+`;
 
-export default MyJobCard
+const JobInfo = styled.p`
+  font-size: 12px;
+  color: #888;
+  margin-bottom: 10px;
+`;
+
+const UpdateStatusButton = styled.button`
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  font-size: 14px;
+  position: absolute;
+  top: 10px; /* Move the button to the top */
+  right: 10px; /* Move the button to the right */
+  cursor: pointer;
+`;
+
+function MyJobCard({ job_application, showIsOn, setShowIsOn }){
+  const { company, description, email, location, phone_number, position, status } = job_application;
+
+  return (
+    <JobCardContainer>
+      <JobStatus>Status: {status}</JobStatus>
+      <JobDescription>{description}</JobDescription>
+      <JobInfo>Position: {position}</JobInfo>
+      <JobInfo>Company: {company}</JobInfo>
+      <JobInfo>Location: {location}</JobInfo>
+      <UpdateStatusButton onClick={()=>setShowIsOn(!showIsOn)}>Update Status</UpdateStatusButton>
+    </JobCardContainer>
+  );
+};
+
+export default MyJobCard;
--- client/src/components/MyJobs.js
+++ client/src/components/MyJobs.js
@@ -1,13 +1,32 @@
 import MyJobCard from "./MyJobCard"
+import styled from 'styled-components'
+import { useState } from "react"
+
+const MyJobsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items: center;
+`;
 
 
 function MyJobs({user}){
-    const displayMyJobCards = !user ? null : user.job_applications.map((job_application)=><MyJobCard job_application={job_application} key={job_application.id}/>)
+    const [showIsOn, setShowIsOn] = useState(false)
+    
+    const displayMyJobCards =
+     !user ? null : user.job_applications.map((job_application)=>
+    <MyJobCard job_application={job_application}
+     key={job_application.id}
+     showIsOn={showIsOn} 
+     setShowIsOn={setShowIsOn}/>)
+     console.log(showIsOn)
 
+     
     return(
-        <div className="my-jobs">
+        <MyJobsContainer>
             {!user ? <h1>Login and get hired!</h1>: (user.job_applications == false ? <h1>Start Applying!!!</h1>: displayMyJobCards)}
-        </div>
+        </MyJobsContainer>
+        
     )
 }
 
