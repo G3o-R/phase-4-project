a4e6e925a7346e0daa59eefe36c21a251a9d016b
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-03 15:28:24 -0700

    building CreateJob.js

--- client/src/components/CreateJob.js
+++ client/src/components/CreateJob.js
@@ -11,11 +11,57 @@ function CreateJob(){
         about_the_job:""
     })
     const {description, pay, location, position, company, about_the_job} = jobFormData
-    return(<div>
-        <h2>Add Jobs Here</h2>
-        <form >
 
-        </form>
+    function handleChange(e){
+        let name = e.target.name
+        let value = e.target.value
+
+        setJobFormData({...jobFormData, [name]:value})
+    }
+
+    function handleCreateJobSubmit(e){
+        e.preventDefault()
+        console.log(jobFormData)
+    }
+
+    return(<div className="add-new-job-form-container">
+        <h2>Add Jobs Here</h2>
+        <form onSubmit={handleCreateJobSubmit}>
+        <label>
+          Description:
+          <input
+            type="text"
+            name="description"
+            value={description}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Pay:
+          <input type="text" name="pay" value={pay} onChange={handleChange}/>
+        </label>
+        <label>
+          Location:
+          <input type="text" name="location" value={location} onChange={handleChange}/>
+        </label>
+        <label>
+          Position:
+          <input type="text" name="position" value={position} onChange={handleChange}/>
+        </label>
+        <label>
+          Company:
+          <input type="text" name="company" value={company} onChange={handleChange}/>
+        </label>
+        <label>
+          About the Job:
+          <textarea
+            name="about_the_job"
+            value={about_the_job}
+            onChange={handleChange}
+          ></textarea>
+        </label>
+        <button type="submit">Create Job!</button>
+      </form>
     </div>)
 }
 
