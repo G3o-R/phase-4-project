c4a3fbc4aab3b8b256a7223e31547b4014bdc98b
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-03 19:00:10 -0700

    errors can be displayed if bad data is offered when providing a job

--- client/src/components/CreateJob.js
+++ client/src/components/CreateJob.js
@@ -1,68 +1,203 @@
-import { useState } from "react"
+import { useContext, useState } from "react";
+import styled from "styled-components";
+import { Context } from "./Context/Context";
 
+const CreateJobContainer = styled.div`
+  background-color: #ffffff;
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  width: 24rem;
+  margin: 20px auto;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  min-height: 60rem;
+`;
 
-function CreateJob(){
-    const [jobFormData, setJobFormData] = useState({
-        description:"",
-        pay: "",
-        location:"",
-        position:"",
-        company:"",
-        about_the_job:""
-    })
-    const {description, pay, location, position, company, about_the_job} = jobFormData
+const FormTitle = styled.h2`
+  font-size: 20px;
+  color: #333;
+  text-align: center;
+  margin-bottom: 20px;
+`;
 
-    function handleChange(e){
-        let name = e.target.name
-        let value = e.target.value
+const FormLabel = styled.label`
+  font-size: 16px;
+  color: #333;
+  text-decoration: underline;
+  display: block;
+  margin-bottom: 8px;
+`;
 
-        setJobFormData({...jobFormData, [name]:value})
-    }
+const FormInput = styled.input`
+  font-size: 16px;
+  padding: 8px;
+  margin: 0.5rem 0;
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+  width: 100%;
+`;
 
-    function handleCreateJobSubmit(e){
-        e.preventDefault()
-        console.log(jobFormData)
-    }
+const FormTextarea = styled.textarea`
+  font-size: 16px;
+  padding: 8px;
+  margin: 0.5rem 0;
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+  width: 100%;
+  min-height: 30rem;
+  resize: none;
+`;
 
-    return(<div className="add-new-job-form-container">
-        <h2>Add Jobs Here</h2>
-        <form onSubmit={handleCreateJobSubmit}>
-        <label>
-          Description:
-          <input
-            type="text"
-            name="description"
-            value={description}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Pay:
-          <input type="text" name="pay" value={pay} onChange={handleChange}/>
-        </label>
-        <label>
-          Location:
-          <input type="text" name="location" value={location} onChange={handleChange}/>
-        </label>
-        <label>
-          Position:
-          <input type="text" name="position" value={position} onChange={handleChange}/>
-        </label>
-        <label>
-          Company:
-          <input type="text" name="company" value={company} onChange={handleChange}/>
-        </label>
-        <label>
-          About the Job:
-          <textarea
-            name="about_the_job"
-            value={about_the_job}
-            onChange={handleChange}
-          ></textarea>
-        </label>
-        <button type="submit">Create Job!</button>
-      </form>
-    </div>)
+const FormButton = styled.button`
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  margin-top: 10px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const ErrorText = styled.p`
+  color: #ff0000;
+  font-weight: bold;
+  margin-right: 10rem;
+`;
+
+const FormAndErrorsContainer = styled.div`
+  width: 70rem;
+  margin: 0 auto; /* Center the container horizontally */
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: flex-start;
+`;
+
+function CreateJob() {
+  const { jobsData, setJobs } = useContext(Context);
+  const [jobFormData, setJobFormData] = useState({
+    description: "",
+    pay: "",
+    location: "",
+    position: "",
+    company: "",
+    about_the_job: "",
+  });
+  const [errors, setErrors] = useState([]);
+
+  const {
+    description,
+    pay,
+    location,
+    position,
+    company,
+    about_the_job,
+  } = jobFormData;
+
+  function handleChange(e) {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setJobFormData({ ...jobFormData, [name]: value });
+  }
+
+  function handleCreateJobSubmit(e) {
+    e.preventDefault();
+    console.log(jobFormData);
+    fetch("/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jobFormData),
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((newJob) => setJobs([...jobsData, newJob]));
+      } else {
+        res.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
+
+  return (
+    <div className="add-job">
+      <FormAndErrorsContainer>
+        <CreateJobContainer>
+          <FormTitle>Add Jobs Here</FormTitle>
+          <form onSubmit={handleCreateJobSubmit}>
+            <FormLabel>
+              Position:
+              <FormInput
+                type="text"
+                name="position"
+                value={position}
+                onChange={handleChange}
+              />
+            </FormLabel>
+            <FormLabel>
+              Description:
+              <FormInput
+                type="text"
+                name="description"
+                value={description}
+                onChange={handleChange}
+              />
+            </FormLabel>
+            <FormLabel>
+              Company:
+              <FormInput
+                type="text"
+                name="company"
+                value={company}
+                onChange={handleChange}
+              />
+            </FormLabel>
+            <FormLabel>
+              Location:
+              <FormInput
+                type="text"
+                name="location"
+                value={location}
+                onChange={handleChange}
+              />
+            </FormLabel>
+            <FormLabel>
+              Pay:
+              <FormInput
+                type="text"
+                name="pay"
+                value={pay}
+                onChange={handleChange}
+              />
+            </FormLabel>
+            <FormLabel>
+              About the Job:
+              <FormTextarea
+                name="about_the_job"
+                value={about_the_job}
+                onChange={handleChange}
+              ></FormTextarea>
+            </FormLabel>
+            <FormButton type="submit">Create Job!</FormButton>
+          </form>
+        </CreateJobContainer>
+        <div>
+          {errors.map((error, index) => (
+            <ErrorText key={index}>* {error}</ErrorText>
+          ))}
+        </div>
+      </FormAndErrorsContainer>
+    </div>
+  );
 }
 
-export default CreateJob
+export default CreateJob;
