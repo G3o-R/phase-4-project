9701b962e47e72bd7a688f2a1c9ac53dc2cdb873
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-06 20:41:12 -0700

    removed an unused route

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   get "/me", to: "users#show"
   delete "/logout", to: "sessions#destroy"
 
-  # make these routes nested later!!!
-  delete "/users/:user_id/job_applications/:id", to: "job_applications#destroy"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
