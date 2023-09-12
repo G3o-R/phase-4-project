734f9cacbdbc2470a9b56b61673967bface2babd
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-12 09:49:18 -0700

    removed index method from job_applications_controller

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -4,10 +4,6 @@ class JobApplicationsController < ApplicationController
         render json: job_applications
     end
 
-    def index
-        render json: JobApplication.all
-    end
-
     def update 
         job_applications = @current_user.job_applications.find(params[:id])
         job_applications.update!(application_params)
