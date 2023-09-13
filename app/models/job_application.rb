599d3c5cac9369790f931c74bd41afdefbaa6416
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-09-12 19:09:09 -0700

    changed validations on jobApplication model so that other multiple users can apply to the same job

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -5,5 +5,5 @@ class JobApplication < ApplicationRecord
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "should be a valid email address" }
   validates :phone_number, presence: true, format: { with: /\A\d{10}\z/, message: "should be a 10-digit number" }
   validates :status, presence: true
-  validates :job_id, presence: true, uniqueness: true
+  validates :user_id, uniqueness: { scope: :job_id, message: "You've already applied to this job." }
 end
