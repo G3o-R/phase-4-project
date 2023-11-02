de24a5b100ef8eac3fe32f6effe02a2ee1347477
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-11-02 10:21:04 -0700

    debugging

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,6 +8,7 @@ npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 # builds the back end code
+
 bundle install
 bundle exec rake db:migrate
 bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
